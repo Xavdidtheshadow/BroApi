f48521a30033bac0cce14c47048c38b09195c420
David Brownman <beamneocube@gmail.com>
2015-10-05 16:04:42 -0700

    fixed for python2

--- BROApi.py
+++ BROApi.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 
 words = ['amigo','bra','brah','bro','broski','brotha','brother','bud','buddy','compadre','dawg','dude','guy','homeboy','homie','mate','pal']
 
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({'message': 'try /bro instead'})
+
 @app.route('/bro', methods=['GET'])
 def broMe():
     return jsonify({'bro': random.choice(words)})
 
 @app.errorhandler(404)
-def 404_error(error):
+def four04_error(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
 
-if __name__ == '__main':
+if __name__ == '__main__':
     app.run()
